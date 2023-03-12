a5897ce8ab013c3ab4e73dee44f857d8c21d70cb
Two-Jay <djeeee1272@gmail.com>
2023-03-12 09:21:31 +0900

    ex03 start

---
+++ ex03/csvreader.py
@@ -0,0 +1,22 @@
+class CsvReader():
+    def __init__(self, filename=None, sep=',', header=False, skip_top=0, skip_bottom=0):
+        pass
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
+
+    def getdata(self):
+        pass
+
+    def getheader(self):
+        pass
+
+
+def main():
+    return 0
+
+if __name__ == "__main__":
+    main()
