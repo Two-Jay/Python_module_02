857741286eeb5cd10223e9cde26acc5ed8b5ba94
Two-Jay <djeeee1272@gmail.com>
2023-04-17 12:06:00 +0900

    update

--- ex00/ft_reduce.py
+++ ex00/ft_reduce.py
@@ -10,8 +10,7 @@ def ft_reduce(function_to_apply, iterables, initial=None):
         value = function_to_apply(value, element)
     return value
 
-if __name__ == "__main__":
-    input_string = "Hello World! Hello Python Module! 0123456789"
-    print(reduce(lambda x, y: x + ord(y), input_string, 0))
-    print(ft_reduce(lambda x, y: x + ord(y), input_string, 0))
-    
+# if __name__ == "__main__":
+#     input_string = "Hello World! Hello Python Module! 0123456789"
+#     print(reduce(lambda x, y: x + ord(y), input_string, 0))
+#     print(ft_reduce(lambda x, y: x + ord(y), input_string, 0))
